04279e147e340066c739c4262d87677204eabb2a
leo <leonard.aronsson@skola.taby.se>
2024-04-24 10:56:19 +0200

    Co-authored-by: Nirrpirre <Nirrpirre@users.noreply.github.com>

---
+++ menu.js
@@ -0,0 +1,3 @@
+document.getElementById("start").addEventListener("click", ()=>{
+    window.location.href="index.html"
+})
--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const canvas = document.getElementById('gameCanvas');
 let c = canvas.getContext('2d')
 canvas.width = window.innerWidth - 50;
 canvas.height = window.innerHeight -5;
+let text = document.getElementById('text')
 
 arr = []
 obstacleWidth = 60
 obstacleHeight = 500
 obstacleX = canvas.width
 obstacleY = 0
+
+let settings = document.getElementById('settings')
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        isPaused = true
+        settings.style.display = 'flex'
+    }
+})
+
+
 
 function addObstacle() {
     let random = obstacleY - obstacleHeight/4 - Math.random()*(obstacleHeight/2)
@@ -18,6 +30,12 @@ function addObstacle() {
         height: obstacleHeight,
     }
     arr.push(top)
+    for (let i = 0; i < arr.length; i++) {
+        let obstacle = arr[i]
+        c.fillStyle = 'black'
+        c.fillRect(top.x, top.y, top.width, top.height)
+        console.log(obstacle)
+    }
 }
 
 let isPaused = false
@@ -86,34 +104,33 @@ class Game {
     }
 
     animate() {
+        // console.log(this.player)
         this.player.updatePosition();
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.player.draw(this.ctx);
-        
-        for (let i = 0; i < arr.length; i++) {
-            let obstacle = arr[i]
-            this.ctx.fillStyle = 'black'
-            this.ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height)
-        }
-        requestAnimationFrame((timestamp) => this.animate(timestamp));
-    }
 
-    start() {
-        document.addEventListener('keypress', (e) => {
-            if (e.key === 'w' && !isPaused) {
-                this.player.y = canvas.height/2
-                this.player.x = canvas.width/2
-                this.player.speedY = 0
-                document.addEventListener('keypress', (e) => {
-                    if (e.key === ' ') {
-                        this.handleclick()
-                    }
-                });
-                requestAnimationFrame((timestamp) => this.animate(timestamp));
-            }
-        })
+
+        requestAnimationFrame(this.animate.bind(this))
     }
 }
 
 const game = new Game(canvas);
-game.start();
+
+document.addEventListener('keypress', (e) => {
+    if (e.key === 'w' && !isPaused) {
+        game.player.y = canvas.height/2
+        game.player.x = canvas.width/2
+        game.player.speedY = 0
+        game.animate();
+        
+    }
+})
+
+document.addEventListener('keypress', (e) => {
+    if (e.key === ' ') {
+        game.handleclick()
+    }
+});
+
+setInterval(addObstacle(), 1);
+
